43c91f827606c1113932b61c7ce9eb678703c6cd
pahadi-guy <vikasuv11@gmail.com>
2021-10-23 00:21:30 +0530

    Header

--- src/home.js
+++ src/home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import banner from "./images/banner.jpg";
+import cart from "./images/cart.png";
 
 import Category from "./Component/Category";
-import { Button, Nav, Navbar, Container } from "react-bootstrap";
+
 function Home() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      {/* <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
@@ -18,9 +18,18 @@ function Home() {
           </Nav>
         </Container>
         <Button>cart</Button>
-      </Navbar>
+      </Navbar> */}
+
       <div className="banner">
-        <img src={banner} alt="Banner" />
+        <div className="header">NIKKU FOOD SERVICE</div>
+        <div className="search">
+          <input placeholder="Search..." />
+          <button>Go</button>
+        </div>
+        <button type="button" class="btn btn-primary">
+          <img src={cart} alt="cart" />
+          <span>0</span>
+        </button>
       </div>
       <Category />
     </>
